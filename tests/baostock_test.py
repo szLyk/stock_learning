ed13177fbf11e6fa60a82fb3f4a01b9dca4e97d0
szlyk <349978892@qq.com>
2026-02-28 21:16:44 +0800

    feature(baostock): 计算CCI、RSI、BOLL、OBV、MACD数据

--- tests/baostock_test.py
+++ tests/baostock_test.py
@@ -5,7 +5,6 @@ if __name__ == '__main__':
     # fetcher = BaostockFetcher()
     # fetcher.init_stock_date_week_month()
     # fetcher.close()
-
     indicator_calculator = IndicatorCalculator()
     indicator_calculator.run_batch_cci_all_time_period()
     indicator_calculator.close()
